b3cc2e7926532ec83ae7399d5b4745b1f13d5b93
GEPEL <shota1130s2k@gmail.com>
2020-01-07 20:33:08 +0900

    ScriptTestプロジェクト
    再提出用に再度スクリプトを見直し、Sceneを再生時Console上でメッセージが表示されるのを確認しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,77 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss{
-	private int hp = 100; //体力
-	private int power = 25; //攻撃力
-	
-	//=====================応用課題用=====================
-	private int mp = 53; //MP
-	//=====================応用課題用（end）=====================
+public class Boss
+{
+    private int hp = 100; //体力
+    private int power = 25; //攻撃力
 
-	// 攻撃用の関数
-	public void Attack() { 
-			Debug.Log( this.power + "のダメージを与えた" );
-	}
+    //=====================応用課題用=====================
+    private int mp = 53; //MP
+                         //=====================応用課題用（end）=====================
 
-	// 防御用の関数
-	public void Defence(int damage) { 
-			Debug.Log( damage+"のダメージを受けた" );
-			// 残りhpを減らす
-			this.hp -= damage;
-	}
-	
-	//=====================応用課題用=====================
-	public void Magic(int mp){
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
 
-		if(this.mp - mp > 0)
-		{
-			this.mp -= mp;
-			Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
-		}else{
-			Debug.Log("MPが足りないため魔法が使えない");
-		}
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
 
-	}
-	//=====================応用課題用（end）=====================
+    //=====================応用課題用=====================
+    public void Magic(int mp)
+    {
+
+        if (this.mp - mp > 0)
+        {
+            this.mp -= mp;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+
+    }
+    //=====================応用課題用（end）=====================
 }
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		//Bossクラスの変数を宣言してインスタンスを代入する
-		Boss lastBoss = new Boss();
+    // Use this for initialization
+    void Start()
+    {
+        //Bossクラスの変数を宣言してインスタンスを代入する
+        Boss lastBoss = new Boss();
 
-		//攻撃用の関数を呼び出す
-		lastBoss.Attack();
-		//防御用の関数を呼び出す
-		lastBoss.Defence(3);
+        //攻撃用の関数を呼び出す
+        lastBoss.Attack();
+        //防御用の関数を呼び出す
+        lastBoss.Defence(3);
 
-		//=====================基本課題用=====================
-		
-		int[] array = {2,5,7,9,11}; //配列の初期化
+        //=====================基本課題用=====================
 
-		for(int i = 0; i < array.Length; i++){ //昇順
-			Debug.Log(array[i]);
-		}
+        int[] array = { 2, 5, 7, 9, 11 }; //配列の初期化
 
-		for(int i = 0; i < array.Length; i++){ //降順
-			Debug.Log(array[array.Length - i - 1]);
-		}
-		//=====================基本課題用(end)=====================
+        for (int i = 0; i < array.Length; i++)
+        { //昇順
+            Debug.Log(array[i]);
+        }
 
-
-		//=====================応用課題用=====================
-		
-		//魔法攻撃用の関数を呼び出す
-		lastBoss.Magic(5);
-
-		for(int i = 0; i < 10; i++){ //魔法攻撃を10回繰り返す
-			lastBoss.Magic(5);
-		}
-		//=====================応用課題用（end）=====================
+        for (int i = 0; i < array.Length; i++)
+        { //降順
+            Debug.Log(array[array.Length - i - 1]);
+        }
+        //=====================基本課題用(end)=====================
 
 
-	}
+        //=====================応用課題用=====================
+
+        //魔法攻撃用の関数を呼び出す
+        lastBoss.Magic(5);
+
+        for (int i = 0; i < 10; i++)
+        { //魔法攻撃を10回繰り返す
+            lastBoss.Magic(5);
+        }
+        //=====================応用課題用（end）=====================
+
+
+    }
 }
